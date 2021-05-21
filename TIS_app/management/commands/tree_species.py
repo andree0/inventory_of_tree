5648f47b7d4318dac84813102912af291b7c59eb
andree0 <jonek.andree@gmail.com>
2021-05-21 12:43:37 +0200

    web scrapping

--- TIS_app/management/commands/tree_species.py
+++ TIS_app/management/commands/tree_species.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 
 from django.core.management.base import BaseCommand
 
+from TIS_app.models import Species
+
 
 class Command(BaseCommand):
     help = 'Add tree species to database. Use web scrapping.'
@@ -11,5 +13,12 @@ class Command(BaseCommand):
         html_text = requests.get(
             'https://fiszkoteka.pl/zestaw/405991-drzewa-polsko-lacinskie').text
         soup = BeautifulSoup(html_text, 'lxml')
-        tr = soup.find_all('tr')
-        
+        div_tag = soup.find_all('div', class_='text')
+        i = 0
+        while i < len(div_tag):
+            one_species = {
+                'name': div_tag[i].text.strip(),
+                'latin_name': div_tag[i + 1].text.strip(),
+            }
+            Species.objects.get_or_create(**one_species)
+            i += 2
